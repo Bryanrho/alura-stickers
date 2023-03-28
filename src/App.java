2fb129c53aef95d5847ed2aac488f11bfc813d5c
Bryan Hoffmann <bryanhoffmanndev@gmail.com>
2023-03-28 16:17:50 -0300

    Segunda aula + desafios

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -22,12 +24,23 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // exibir e manipular os dados
+        GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
         for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println("\u001b[0mTítulo: \033[1;90m\u001b[47m\t" + filme.get("title") + "\u001b[0m");
-            System.out.println("\u001b[0mPoster: \u001b[34m\u001b[4m\t" + filme.get("image") + "\u001b[0m");
-            System.out.print("\u001b[0mClassificação:  \033[1;90m" + filme.get("imDbRating") + "\u001b[0m ");
+            String urlImage = filme.get("image");
+            String fullTitle = filme.get("fullTitle");
+            String filmName = fullTitle + ".png";
+            String archiveName = filmName.replace(":", "-");
+            String imdbRating = filme.get("imDbRating");
 
-            double doubleRating = Double.valueOf(filme.get("imDbRating"));
+            InputStream inputStream = new URL(urlImage).openStream();
+
+            geradora.cria(inputStream, archiveName,imdbRating);
+
+            System.out.println("\u001b[0mTítulo: \033[1;90m\u001b[47m\t" + fullTitle + "\u001b[0m");
+            System.out.println("\u001b[0mPoster: \u001b[34m\u001b[4m\t" + urlImage + "\u001b[0m");
+            System.out.print("\u001b[0mClassificação:  \033[1;90m" + imdbRating + "\u001b[0m ");
+
+            double doubleRating = Double.valueOf(imdbRating);
             int intRating = (int) doubleRating;
 
             for (int i = 0; i < intRating; i++) {
